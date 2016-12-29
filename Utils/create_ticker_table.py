970dc536f0dfb9ec0b72af522cb31387648cf9bb
Truls Unstad <truls.unstad@gmail.com>
2016-12-29 10:43:54 +0100

    Now actually inserts data and avoids empty entries

--- Utils/create_ticker_table.py
+++ Utils/create_ticker_table.py
@@ -5,18 +5,21 @@ import sys
 from os import path
 
 def create_ticker_entry(ticker, name, cursor):
-	cursor.execute('''INSERT OR IGNORE INTO ticker(ticker,name) VALUES(?,?)''',
-		(ticker, name))
+	ticker = ticker.rstrip()
+	name = name.rstrip()
+	if ticker and name:
+		cursor.execute('''INSERT OR IGNORE INTO ticker(ticker,name) VALUES(?,?)''',
+			(ticker, name))
 	return
 
 # Check command line parameters
 if len(sys.argv) != 2:
-	print 'Usage: download_stockdata list_of_companies'
+	print 'Usage: create_ticker_table list_of_companies'
 	print '       Where the list of companies is expected to'
 	print '       Be in the format of <ticker, name>'
 	sys.exit()
 
-#
+# Check that the input file exists
 company_file = sys.argv[1]
 if not path.exists(company_file):
 	print 'Cannot find company file: ', company_file
@@ -24,11 +27,12 @@ if not path.exists(company_file):
 
 datadir = 'Data'
 
+# Check that the target directory exists
 if not path.isdir(datadir):
 	print 'Cannot find data directory: ', datadir
 	sys.exit()
 
-# Create or open a database file called mydb with a SQLite3 DB
+# Create/open database and set text type to python strings
 db = sqlite3.connect(datadir + '/TickerList.sqlite3')
 db.text_factory = str
 
@@ -41,6 +45,6 @@ with open(company_file, 'r') as tickerfile:
 	for line in tickerfile:
 		ticker, name = line.split(",")[:2]
 		create_ticker_entry(ticker, name, cursor)
-	cursor.execute()
+db.commit()
 
 print "Done"
